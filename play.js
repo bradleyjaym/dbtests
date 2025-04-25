3fd835bbc108ad363bc86af50020e1b879cfc4cd
Bradley Mendez <bradleymendez2014@gmail.com>
2025-04-24 20:43:54 -0700

    first release

---
+++ play.js
@@ -0,0 +1,12 @@
+const myProdObject = {
+  prod_id: 101,
+  prod_name: "Pen",
+  prod_desc: "Black Ink",
+  prod_price: "$0.99"
+}
+
+
+const myProdArray = Object.values(myProdObject)
+console.log(myProdArray);
+
+// Run using Node
